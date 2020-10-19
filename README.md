33f45183f6c09ab83a744c8a02d269ae32ea15c7
ICEY <icey@icey.tech>
2020-10-19 11:42:57 +0800

    Hahafuck

---
+++ README.md
@@ -0,0 +1,14 @@
+# Project-47 2021 plan
+
+Since the previous Pr47 has failed due to certain reason, the plan for 47 project has changed a little. Here is our new plan.
+
+## Stage-1: build the `T10` experimental project
+At this stage we will re-examine the memory model of `pr47`, write several new data strcutures for function and data type bindings, and try them out with manual code instead of procedural macros.
+
+## Stage-2: build the `su47` project
+At this stage we'll write the parser/ast/vm of this project, fully implement the binding part and write the binding procedural macro.
+
+## Stage-3: complete the ecosystem of `su47`
+
+## Stage-4: wait for something like `unsafe_type_id`
+Once rust introduces such features, procedural macro will not be necessary. Binding functions is just as easy as what user does in `rhai` or `gluon`.
---
+++ src/func.rs
@@ -0,0 +1,63 @@
+use std::any::TypeId;
+use std::marker::PhantomData;
+
+pub enum Storage {
+    VMOwned,
+    SharedWithHost,
+    MutSharedWithHost
+}
+
+pub enum RustArgStrategy {
+    Move, Copy, Share, MutShare
+}
+
+pub struct Ptr<'a> {
+    data: *mut (),
+    pub type_info: TypeId,
+    pub storage: Storage,
+    _phantom: PhantomData<&'a ()>
+}
+
+impl<'a> Ptr<'a> {
+    pub fn new(data: *mut (), type_info: TypeId, storage: Storage) -> Self {
+        Self {
+            data,
+            type_info,
+            storage,
+            _phantom: PhantomData::default()
+        }
+    }
+}
+
+pub trait RustCallable<'a> {
+    fn is_unsafe(&self) -> bool;
+
+    fn param_specs(&self) -> &'static [(TypeId, RustArgStrategy)];
+
+    unsafe fn call_prechecked(&self, args: &[Ptr<'a>]) -> Ptr<'a>;
+
+    fn call(&self, args: &[Ptr<'a>]) -> Result<Ptr<'a>, &'static str> {
+        let param_spec = self.param_specs();
+        if param_spec.len() != args.len() {
+            return Err("incorrect argument count")
+        }
+
+        for (arg, (param_type, param_strategy)) in args.iter().zip(param_spec.iter()) {
+            if arg.type_info() != param_type {
+                return Err("incorrect argument type")
+            }
+
+            let _ = match (&arg.storage, param_strategy) {
+                (Storage::VMOwned, _) => PhantomData::default(),
+                (Storage::SharedWithHost, RustArgStrategy::Share) => PhantomData::default(),
+                (Storage::MutSharedWithHost, RustArgStrategy::Share) => PhantomData::default(),
+                (Storage::MutSharedWithHost, RustArgStrategy::MutShare) => PhantomData::default(),
+                _ => return Err("other lifetime error")
+            };
+        }
+
+        unsafe {
+            Ok(self.call_prechecked(args))
+        }
+    }
+}
