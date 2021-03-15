29db08d4bc2d00e152d8cb75084dc7c1346073bc
ICEY <icey@icey.tech>
2021-03-15 13:26:37 +0800

    窗外韭花开 你还记得吗

--- src/intake/mod.rs
+++ src/intake/mod.rs
@@ -1,12 +1,18 @@
-use crate::data::{StaticWrapper, Value};
 use std::mem::MaybeUninit;
+use crate::data::{StaticWrapper, Value};
 
 mod ramped;
 mod oxtank;
 
-pub trait Intake<'a> {
-    fn allocate<T>(&mut self) -> StaticWrapper<T>;
+pub trait OxProvider<'a> {
+    fn allocate<T>(&mut self, t: T) -> StaticWrapper<T>;
+    fn allocate_ref<T>(&mut self, t: &T) -> StaticWrapper<T>;
+    fn allocate_ref_mut<T>(&mut self, t: &mut T) -> StaticWrapper<T>;
+
     fn force_collect(&mut self);
+
     fn add_stack(&mut self, stack: &'a Vec<MaybeUninit<Value>>);
-    fn mark_permanent(&mut self, value: Value);
+
+    fn enable_collect(&mut self);
+    fn disable_collect(&mut self);
 }
--- src/intake/ramped.rs
+++ src/intake/ramped.rs
@@ -1,29 +1,70 @@
+use std::collections::HashSet;
 use std::mem::MaybeUninit;
-use crate::data::{Value, StaticWrapper};
-use crate::intake::Intake;
+use crate::data::{StaticWrapper, Value};
+use crate::intake::OxProvider;
+
+pub struct IntakeOptions {
+    max_debt: u32,
+    max_debt_bytes: usize
+}
+
+impl IntakeOptions {
+    pub fn new(max_debt: u32, max_debt_bytes: usize) -> Self {
+        Self {
+            max_debt,
+            max_debt_bytes
+        }
+    }
+}
+
+impl Default for IntakeOptions {
+    fn default() -> Self {
+        Self::new(1024, 32768)
+    }
+}
 
 pub struct RampedIntake<'a> {
+    options: IntakeOptions,
+
     stacks: Vec<&'a Vec<MaybeUninit<Value>>>,
-    heap: Vec<*mut Value>
+    heap: Vec<*mut Value>,
+
+    collect_enabled: bool,
+    debt: u32,
+    debt_bytes: usize,
 }
 
 impl<'a> RampedIntake<'a> {
-    pub fn new() -> Self {
+    pub fn new(options: IntakeOptions) -> Self {
         Self {
+            options,
+
             stacks: Vec::new(),
-            heap: Vec::new()
+            heap: Vec::new(),
+
+            collect_enabled: false,
+            debt: 0,
+            debt_bytes: usize
         }
     }
 }
 
 impl<'a> Default for RampedIntake<'a> {
     fn default() -> Self {
-        Self::new()
+        Self::new(IntakeOptions::default())
     }
 }
 
-impl<'a> Intake<'a> for RampedIntake<'a> {
-    fn allocate<T>(&mut self) -> StaticWrapper<T> {
+impl<'a> OxProvider<'a> for RampedIntake<'a> {
+    fn allocate<T>(&mut self, _t: T) -> StaticWrapper<T> {
+        todo!()
+    }
+
+    fn allocate_ref<T>(&mut self, _t: &T) -> StaticWrapper<T> {
+        todo!()
+    }
+
+    fn allocate_ref_mut<T>(&mut self, _t: &mut T) -> StaticWrapper<T> {
         todo!()
     }
 
@@ -32,10 +73,14 @@ impl<'a> Intake<'a> for RampedIntake<'a> {
     }
 
     fn add_stack(&mut self, stack: &'a Vec<MaybeUninit<Value>>) {
-        todo!()
+        self.stacks.push(stack);
     }
 
-    fn mark_permanent(&mut self, value: Value) {
-        todo!()
+    fn enable_collect(&mut self) {
+        self.collect_enabled = true;
+    }
+
+    fn disable_collect(&mut self) {
+        self.collect_enabled = false;
     }
 }
